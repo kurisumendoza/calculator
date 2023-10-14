28a116986ec4da7b8a65f650d0c727b19df104c4
kurisu26 <creative.kristopher@gmail.com>
2023-10-14 22:48:02 +0800

    Create a function that takes an operator and calls one math function

--- script/script.js
+++ script/script.js
@@ -1,23 +1,42 @@
-function add(a, b) {
-  return a + b;
+let firstNumber;
+let secondNumber;
+let operator;
+
+const operations = [
+  {
+    symbol: '+',
+    function: function add(a, b) {
+      return a + b;
+    }
+  },
+  {
+    symbol: '-',
+    function: function subtract(a, b) {
+      return a - b;
+    }
+  },
+  {
+    symbol: '*',
+    function: function multiply(a, b) {
+      return a * b;
+    }
+  },
+  {
+    symbol: '/',
+    function: function divide(a, b) {
+      return a / b;
+    }
+  }
+]
+
+function operate(operator, a, b) {
+  let result;
+  operations.forEach(operation => {
+    if (operation.symbol === operator) {
+      result = operation.function(a, b);
+    }
+  })
+  return result;
 }
 
-console.log(add(1, 2));
-
-function subtract(a, b) {
-  return a - b;
-}
-
-console.log(subtract(1, 2));
-
-function multiply(a, b) {
-  return a * b;
-}
-
-console.log(multiply(1, 2));
-
-function divide(a, b) {
-  return a / b;
-}
-
-console.log(divide(1, 2));
+console.log(operate('/', 1, 2));
